1b52828345ec325e31520073613ef4fd64a5cd82
aniki <aniki@MacBook-Air-de-Brahim.local>
2021-12-22 19:23:00 +0100

    exercice 1 OK

--- src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
+++ src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 
 @Component({
-  selector: 'app-exercice1-enfant',
-  templateUrl: './exercice1-enfant.component.html',
-  styleUrls: ['./exercice1-enfant.component.css']
+  selector: "app-exercice1-enfant",
+  templateUrl: "./exercice1-enfant.component.html",
+  styleUrls: ["./exercice1-enfant.component.css"],
 })
 export class Exercice1EnfantComponent implements OnInit {
+  @Input() public counter = 0;
+  @Output() private changeCounter: EventEmitter<number> = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  public incr(): void {
+    this.changeCounter.emit(1);
   }
 
+  public decr(): void {
+    this.changeCounter.emit(-1);
+  }
 }
