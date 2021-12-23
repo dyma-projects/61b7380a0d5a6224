d88f5879e291e251809ddd308c31f717e8dc885b
aniki <aniki@MacBook-Air-de-Brahim.local>
2021-12-23 16:15:09 +0100

    exercice 2 ok

--- src/app/exercice2/exercice2.component.ts
+++ src/app/exercice2/exercice2.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 
 @Component({
-  selector: 'app-exercice2',
-  templateUrl: './exercice2.component.html',
-  styleUrls: ['./exercice2.component.css']
+  selector: "app-exercice2",
+  templateUrl: "./exercice2.component.html",
+  styleUrls: ["./exercice2.component.css"],
 })
 export class Exercice2Component implements OnInit {
   public valeur: string;
+  @ViewChild("myinput") public inputElement: ElementRef<HTMLInputElement>;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  public changeValue(): void {
+    this.valeur = this.inputElement.nativeElement.value;
   }
-
 }
